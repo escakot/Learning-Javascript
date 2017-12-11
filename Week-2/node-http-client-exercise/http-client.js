84e34034fc56ce23295feda4ccc7a92e28a7b66f
escakot <ec@neorennie.com>
2017-12-11 17:53:01 -0500

    Completed Step 2

--- Week-2/node-http-client-exercise/http-client.js
+++ Week-2/node-http-client-exercise/http-client.js
@@ -18,10 +18,35 @@ function getAndPrintHTMLChunks () {
       console.log('\n');
     })
 
-    response.on('end', function(data) {
+    response.on('end', function() {
       console.log("Completed Printing.")
     })
   })
 }
 
-getAndPrintHTMLChunks();
+// getAndPrintHTMLChunks();
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  var htmlData
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+
+    response.on('data', function(data) {
+      htmlData += data
+    })
+
+    response.on('end', function() {
+      console.log(htmlData);
+    })
+  })
+}
+
+getAndPrintHTML();
